1f030cc4afa89a3e55eb910ec3c46d91effbb8e2
AndreyGromov <dikayabebra16@gmail.com>
2024-05-30 20:09:30 +0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,3 @@
-
-
 const player = document.querySelector('.player'),
         playBtn = document.querySelector('.play'),
         prevBtn = document.querySelector('.prev'),
@@ -59,15 +57,47 @@ progressContainer.addEventListener('click', setProgress)
 document.getElementById('closePopupPlayer').addEventListener('click', function() {
 
     var closeSong = document.getElementById('playerThis');
-
     closeSong.classList.remove('show')
 });
 
 document.getElementById('thissong').addEventListener('click', function() {
 
     var popupSong = document.getElementById('playerThis');
-
     popupSong.classList.toggle('show');
+});
+
+// Кнопки и их секции
+document.addEventListener("DOMContentLoaded", function () {
+    const sectionButtons = document.querySelectorAll(".sectionButton");
+    const selectionButtons = document.querySelectorAll(".selectionButton");
+    const sections = document.querySelectorAll(".section");
+    const sectionSelection = document.querySelectorAll(".sectionSelection");
+
+    sectionButtons.forEach(sectionButton => {
+        sectionButton.addEventListener("click", () => {
+            // Удаляем класс active у всех кнопок и секций
+            sectionButtons.forEach(btn => btn.classList.remove("active"));
+            sections.forEach(section => section.classList.remove("active"));
+            // Добавляем класс active к текущей кнопке и соответствующей секции
+            sectionButton.classList.add("active");
+            const target = sectionButton.getAttribute("data-target");
+            document.getElementById(target).classList.add("active");
+        });
+    });
+
+    //фильтр
+        selectionButtons.forEach(selectionButton => {
+        selectionButton.addEventListener("click", () => {
+            selectionButtons.forEach(btn => btn.classList.remove("activeSelect"));
+            sectionSelection.forEach(section => section.classList.remove("active"));
+            selectionButton.classList.add("activeSelect");
+            const target = selectionButton.getAttribute("data-target");
+            document.getElementById(target).classList.add("active");
+        });
+    });
+
+
+
 });
 
 let tg = window.Telegram.WebApp;
